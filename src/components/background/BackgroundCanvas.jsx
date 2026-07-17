fb78fa6dfb46aa4e2655c9d4ed5052ec603947c4
HellManon <manon.hell@outlook.fr>
2026-07-17 17:04:40 +0200

    Add background canvas

--- src/components/background/BackgroundCanvas.jsx
+++ src/components/background/BackgroundCanvas.jsx
@@ -1,7 +1,12 @@
 import { Canvas } from "@react-three/fiber";
 
 function BackgroundCanvas() {
-    return <Canvas></Canvas>;
+    return <Canvas>
+    <mesh>
+        <boxGeometry />
+        <meshStandardMaterial />
+    </mesh>
+</Canvas>;
 }
 
 export default BackgroundCanvas;
