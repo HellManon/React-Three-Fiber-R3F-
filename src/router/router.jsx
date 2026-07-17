52f9934649b870c86344161557d35ef4aaa76bbf
HellManon <manon.hell@outlook.fr>
2026-07-17 16:36:56 +0200

    Configure React Router

---
+++ src/router/router.jsx
@@ -0,0 +1,32 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import Home from "../pages/Home";
+import Projects from "../pages/Projects";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import NotFound from "../pages/NotFound";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "/projects",
+        element: <Projects />,
+    },
+    {
+        path: "/about",
+        element: <About />,
+    },
+    {
+        path: "/contact",
+        element: <Contact />,
+    },
+    {
+        path: "*",
+        element: <NotFound />,
+    },
+]);
+
+export default router;
